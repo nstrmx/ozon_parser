84570233e97d4ea491741475ce866b2de39f5179
ckounter <gladunov.denis@gmail.com>
2022-05-18 03:37:30 +0300

    Upgraded log function into a more customizable variant. Had some battle with mypy typing system (partial_wrapper). Renamed request_wrapper to exception handler and removed inner function nesting.

--- utils.py
+++ utils.py
@@ -1,13 +1,96 @@
-from typing import Any
+from typing import Any, Union, Optional, List, Tuple, Dict, Callable, NewType
+from types import FunctionType
 
-import datetime as dt
-from settings import DEBUG, VERBOSE_LOG
+from datetime import datetime
+from functools import partial
+from argparse import Namespace
+from pprint import pprint
+
+from settings import LOGGING_ENABLED, LEVELS, INFO, ERROR, VERBOSITY_LEVEL
 
 
 
-def log(*args, level: str = "INFO", **kwargs):
-    if DEBUG == True and VERBOSE_LOG == True:
-        print(f"[{dt.datetime.now()}][{level}]:", *args, **kwargs)
+
+types = Namespace()
+ExceptionArgs = NewType('ExceptionArgs', Tuple[Exception])
+ExceptionHandler = Callable[[ExceptionArgs, str], Any]
+
+
+def partial_wrapper(*part_args, **part_kwargs):
+    def wrapper_func(func):
+        return partial(func, *part_args, **part_kwargs)
+    return wrapper_func
+
+
+
+def __log(*args,
+        enabled: bool       = True, 
+        levels: List[str]   = [],
+        level: int          = 0,
+        verbosity: int      = 0,
+        printer: Callable   = print,
+        **kwargs):
+    """
+    parameters
+        level: indicates given level of a log message
+        verbosity: only messages with given (and higher) verbosity level will be displayed
+        levels: list of log level names to work with
+        debug: enables/disables logging
+    """
+
+    if enabled == True:
+        if level >= verbosity:
+            print(f"[{datetime.now()}][{levels[level]}]:", end=' ')
+            printer(*args, **kwargs)
+
+
+def log(*args, **kwargs):
+    return __log(*args, 
+                 enabled = LOGGING_ENABLED, 
+                 verbosity = VERBOSITY_LEVEL, 
+                 levels = LEVELS, 
+                 **kwargs)
+
 
 def hasmethod(obj: Any, method_name: str) -> bool:
     return hasattr(obj, method_name) and callable(getattr(obj, method_name))
+    
+
+def default_handler(exception: Exception, message: str) -> Any:
+    exception_name = str(exception)
+    log(f"({exception_name})", message, level=ERROR)
+
+
+def exception_handler(exception: Union[Exception, FunctionType] = Exception, 
+                      handler: ExceptionHandler = default_handler) -> Callable:
+
+    __func: FunctionType = lambda *args, **kwargs: None
+
+    def wrapper_proc(*args, **kwargs) -> Any:
+        nonlocal exception
+        nonlocal handler
+    
+        try:
+            return __func(*args, **kwargs)
+        except exception as e:
+            return handler(exception, e, *args, **kwargs)
+            
+
+    def wrapper_func(func: Callable) -> FunctionType:
+        nonlocal __func
+
+        __func = func
+        return wrapper_proc
+        
+    
+    if isinstance(exception, FunctionType):
+        __func = exception
+        exception = Exception
+        return wrapper_proc
+
+    return wrapper_func
+
+
+def response_printer(*args, **kwargs):
+    print()
+    pprint(*args, **kwargs)
