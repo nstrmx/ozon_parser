b96e46120da3773e6ab5e21bdf9f5ffab6419e0b
ckounter <gladunov.denis@gmail.com>
2023-10-22 08:34:45 +0300

    fix gsript

--- gscript.js
+++ gscript.js
@@ -14,27 +14,26 @@ function calc_diff () {
         percent    = list1.getRange("I2:I"),
         cell_a, cell_b, val_a, val_b,
         i;
-
     var diff_values = []
     var perc_values = []
-
     for (i = 0; i < new_price.length; i++) {
         cell_a = new_price[i][0]
         cell_b = old_price[i][0]
         if (cell_a && cell_b) {
-            val_a = parseInt(cell_a.replace(/(₽|\s)/, ""))
-            val_b = parseInt(cell_b.replace(/(₽|\s)/, ""))
+            val_a = parseInt(cell_a)
+            val_b = parseInt(cell_b)
 
-            diff_values.push([val_a - val_b])
-            perc_values.push([(100 - Math.round(val_a / val_b * 100)) + "%"])
+            max_val = Math.max(val_a, val_b)
+            min_val = Math.min(val_a, val_b)
+
+            diff_values.push([max_val - min_val])
+            perc_values.push([(100 - Math.round(min_val / max_val * 100)) + "%"])
         }
         else {
             diff_values.push([""])
             perc_values.push([""])
         }
-        
     }
-
     difference.setValues(diff_values)
     percent.setValues(perc_values)
 }
